0cbc6fbd76938b12680e7e2eb1ff7e9ee114e3af
Robin Schaufler <robin@likethebird.com>
2018-02-20 11:13:06 -0500

    Harness infrastructure
    - harness/index.html for running a demo-test
    - harness/app.js to bridge index.html to the fusionApi JS
    - harness/webpack.config.js to configure webpack for building the
      demo-test harness
    - harness/build.sh runs webpack

---
+++ harness/app.js
@@ -0,0 +1,17 @@
+"use strict";
+/* jshint browser: true */
+
+function start() {
+//    var fusionApiAdata = {
+//        fusiontable: '10Uc_t_dBYUV_K_j6HdCMSgLXGs94bXvunQBGDOjF',
+//        googleApiKey: 'AIzaSyDFtlZBiU0ZzSrotn4WnPTjWg9g33EZ5Po'
+//    };
+
+    console.log('start');
+    var elems = {
+        passed: document.getElementById('passed'),
+        inWork: document.getElementById('in-work')
+    };
+    console.log('passed elem is ' + elems.passed.innerHTML);
+}
+window.addEventListener('load', start, false);
